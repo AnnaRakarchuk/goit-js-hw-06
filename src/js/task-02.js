4aa0046ef75b0741c1e07ab76187b33e5bd3a052
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-08-21 20:05:06 +0300

    Add decision of task 01 and task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('ul');
+
+const elements = [];
+for(let i = 0; i < ingredients.length; i+= 1 ){
+const elem = ingredients[i];
+
+const ilEl = document.createElement('li');
+ilEl.textContent = elem;
+ilEl.classList.add('item');
+
+elements.push(ilEl);
+}
+
+
+
+ulEl.append(...elements);
+
